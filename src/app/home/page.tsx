4202324e461de1f2023d696181a6873556ca4c3a
@7Steng7 <black-veil77@hotmail.com>
2025-02-10 16:15:25 -0500

    move timon to presentation and add favicon

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import React, { useState } from 'react';
-import Timon from '../Timon/timon';
+import React from 'react';
+import Presentation from '../presentation/presentation';
 import Projects from '../projects/page';
 import Skills from '../skills/page';
 import Aboutme from '../about/page';
@@ -9,15 +9,7 @@ import Contact from '../contact/page';
 export default function Home() {
   return (
     <>
-      <section className="flex flex-col justify-center items-center bg-darkSeaGreen rounded-lg md:items-start md:pl-16 md:items-start lg:pl-0 lg:flex-row lg:items-center lg:pl-16 mt-16">
-        <Timon />
-        <div className='w-full flex flex-col justify-center items-center md:flex md:justify-end md:flex-col md:relative md:w-3/4 md:-translate-y-12 md:translate-x-40 md:bg-deepNavyBlue md:bg-opacity-80 md:rounded-lg md: p-4 md:text-left lg:p-8 lg:w-1/2 lg:-translate-y-0 lg:translate-x-0 lg:m-16 md:transform md:transition-all md:duration-300 md:hover:scale-105 md:hover:shadow-lg'>
-        <h1 className='text-center font-bold text-3xl mb-6 md:text-right lg:text-center w-full'>🧭 Con dirección clara, cada objetivo se convierte en destino.</h1>
-          <p className="text-center w-60 mb-6 text-lg p-4 md:text-right md:mb-0 lg:text-center w-full">
-            ¡Ahoy! Soy Steng, un desarrollador web y capitán fullstack que navega por los mares del código y la creatividad. Desde diseñar interfaces modernas con React y Next.js hasta desarrollar robustas APIs y arquitecturas backend con Node.js, transformo ideas en soluciones digitales que conquistan cualquier desafío.
-          </p>
-        </div>
-      </section>
+      <Presentation />
       <Aboutme />
       <Skills />
       <Projects />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import Header from "./Header/header";
 export const metadata: Metadata = {
   title: "Steng's resume",
   description: "Steng's Portfolio",
+  icons: {
+    icon: "/assets/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -13,9 +16,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <head>
-      <link href="https://fonts.googleapis.com/css2?family=Charm:wght@400;700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Charm:wght@400;700&display=swap" rel="stylesheet"/>
       </head>
       <body
         className={`antialiased bg-deepNavyBlue text-foreground rounded-md shadow-md`}
